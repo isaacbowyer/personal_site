5cad894e6c23c7f0bc67425862a579731ad222a8
Isaac Bowyer <isaacbowyer@googlemail.com>
2025-05-12 19:21:32 +0100

    Product Initalization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Provider } from "../components/ui/provider";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider>
+      <Head>
+        <title>Isaac Bowyer</title>
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width"
+          charSet="utf-8"
+        />
+
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
